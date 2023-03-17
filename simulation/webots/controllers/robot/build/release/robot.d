300536716b8a5486a79eea869790c87c1ea76531
ljyws <945706034@qq,com>
2023-03-17 19:29:03 +0800

    update

--- simulation/webots/controllers/robot/build/release/robot.d
+++ simulation/webots/controllers/robot/build/release/robot.d
@@ -1,4 +1,5 @@
 build/release/robot.o: robot.c \
  E:\webots\Webots/include/controller/c/webots/robot.h \
  E:\webots\Webots/include/controller/c/webots/types.h \
- E:\webots\Webots/include/controller/c/webots/nodes.h
+ E:\webots\Webots/include/controller/c/webots/nodes.h \
+ E:\webots\Webots/include/controller/c/webots/motor.h
--- simulation/webots/controllers/robot/robot.c
+++ simulation/webots/controllers/robot/robot.c
@@ -1,56 +1,19 @@
-/*
- * File:          robot.c
- * Date:
- * Description:
- * Author:
- * Modifications:
- */
 
-/*
- * You may need to add include files like <webots/distance_sensor.h> or
- * <webots/motor.h>, etc.
- */
 #include <webots/robot.h>
+#include <webots/motor.h>
 
 
-/*
- * You may want to add macros here.
- */
 #define TIME_STEP 64
 
-/*
- * This is the main program.
- * The arguments of the main function can be specified by the
- * "controllerArgs" field of the Robot node
- */
-int main(int argc, char **argv) {
-  /* necessary to initialize webots stuff */
+
+int main(int argc, char **argv) 
+{
+
   wb_robot_init();
 
-  /*
-   * You should declare here WbDeviceTag variables for storing
-   * robot devices like this:
-   *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
-   *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
-   */
+  while (wb_robot_step(TIME_STEP) != -1) 
+  {
 
-  /* main loop
-   * Perform simulation steps of TIME_STEP milliseconds
-   * and leave the loop when the simulation is over
-   */
-  while (wb_robot_step(TIME_STEP) != -1) {
-    /*
-     * Read the sensors :
-     * Enter here functions to read sensor data, like:
-     *  double val = wb_distance_sensor_get_value(my_sensor);
-     */
-
-    /* Process sensor data here */
-
-    /*
-     * Enter here functions to send actuator commands, like:
-     * wb_motor_set_position(my_actuator, 10.0);
-     */
   };
 
   /* Enter your cleanup code here */
